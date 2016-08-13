5c112a0388f99f8961fd364728f77aea4053c061
Yuan Lunchuan <799652013@qq.com>
2016-08-13 20:38:46 +0800

    1, update access analyze.

--- access_analyze/access_analyze.py
+++ access_analyze/access_analyze.py
@@ -6,14 +6,10 @@ line = f.readline()
 result = {}
 while line:
     line = f.readline()
-    msg = "----------:"+str(i)+"------: "+line.split(' - - ')[0]
     if line.split(' - - ')[0] in result:
         result[line.split(' - - ')[0]] = result[line.split(' - - ')[0]]+1
     else:
         result[line.split(' - - ')[0]] = 0
-    print(msg)
     i += 1
 print(result)
 f.close()
-
-
