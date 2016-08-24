59e2fda9215c50fa0dcb07fa27e2d9f85ec61309
Yuan Lunchuan <799652013@qq.com>
2016-08-25 00:04:06 +0800

    1, Add doctor crawl.

--- huaxi_doctor/huaxi_doctor_spider.py
+++ huaxi_doctor/huaxi_doctor_spider.py
@@ -1,13 +1,24 @@
 import requests
 from lxml import etree
 
-# base_url = "http://www.cd120.com/findDoctorListByRegService.jspx?regCode=33"
-# html = requests.get(base_url)
-f = open('huaxi_html.txt', 'r', encoding='utf-8')
-html = f.read()
-f.close()
+def urls():
+    urls = []
+    for i in [1, 2, 3, 4, 5]:
+        urls.append("http://www.cd120.com/findDoctorListByRegService.jspx?pageNo="+str(i)+"&regCode=33")
+    return urls
 
-selector = etree.HTML(html)
-doctors = selector.xpath("//tr[@id='ret']")
-for doctor in doctors:
-  print("-----------", doctor.xpath("td[2]/span/a/i/text()")[0])
+def get_html_source(url):
+    return requests.get(url).text
+
+def parse_html(html):
+    selector = etree.HTML(html)
+    doctors = selector.xpath("//tr[@id='ret']")
+    i = 1
+    for doctor in doctors:
+      print(i, doctor.xpath("td[2]/span/a/i/text()")[0])
+      i += 1
+
+if __name__ == '__main__':
+    url_collection = urls()
+    for url in url_collection:
+      parse_html(get_html_source(url))
