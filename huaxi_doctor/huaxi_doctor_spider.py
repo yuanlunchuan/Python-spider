cde0807996cb5e694e691b63bd03f37a9bab276b
Yuan Lunchuan <799652013@qq.com>
2016-08-24 23:44:16 +0800

    1, Add huaxi spider.

--- huaxi_doctor/huaxi_doctor_spider.py
+++ huaxi_doctor/huaxi_doctor_spider.py
@@ -1,6 +1,13 @@
 import requests
+from lxml import etree
 
-base_url = "http://www.cd120.com/findDoctorListByRegService.jspx?regCode=33"
-html = requests.get(base_url)
-print("------------start crawl")
-print(html.text)
+# base_url = "http://www.cd120.com/findDoctorListByRegService.jspx?regCode=33"
+# html = requests.get(base_url)
+f = open('huaxi_html.txt', 'r', encoding='utf-8')
+html = f.read()
+f.close()
+
+selector = etree.HTML(html)
+doctors = selector.xpath("//tr[@id='ret']")
+for doctor in doctors:
+  print("-----------", doctor.xpath("td[2]/span/a/i/text()")[0])
