1b686ff39409b069a8a4035777fbaf42fdb5f19f
Yuan Lunchuan <799652013@qq.com>
2016-10-07 23:06:04 +0800

    1, Add insert method.

--- mongodb_spider/mongodb_test.py
+++ mongodb_spider/mongodb_test.py
@@ -3,5 +3,11 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.huaxiHospital
 collection = db.Doctor
+
+item = {
+  'name': 'zhangsan',
+  'age': 29
+}
+collection.insert(item)
 for doctor in collection.find():
   print('------', doctor)
