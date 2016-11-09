b25decc886b1718e4a2cddc2f1c6fa6a26aba822
Yuan Lunchuan <799652013@qq.com>
2016-11-09 23:17:19 +0800

    1, get movie name.

--- bt_spider/bt_spider.py
+++ bt_spider/bt_spider.py
@@ -13,9 +13,9 @@ class BtSpider:
 
   def get_btn_item(self, html_page):
     selector = etree.HTML(html_page)
-    movies = selector.xpath("//div[@class='item cl']")
+    movies = selector.xpath("//div[@class='title']")
     for movie in movies:
-      print("---------电影名称： "+movie.xpath("div[0]/p/span/font/text()")[0])
+      print("---------电影名称： "+str(movie.xpath("p[1]/a/b/font/text()")))
 
 if __name__=="__main__":
   bt_spider = BtSpider()
