4deb6289b1d155705d7b27147d8428dfb3d1c4d1
Yuan Lunchuan <799652013@qq.com>
2016-11-09 23:57:46 +0800

    1, Add movie info.

--- bt_spider/bt_spider.py
+++ bt_spider/bt_spider.py
@@ -17,7 +17,11 @@ class BtSpider:
     selector = etree.HTML(html_page)
     movies = selector.xpath("//div[@class='title']")
     for movie in movies:
-      print(str(movie.xpath("p[1]/a/b/font")[0].xpath("string(.)"))+"  "+str(movie.xpath("p[1]/span/font/text()")[0]))
+      print("--------------------------")
+      print("电影名称 "+str(movie.xpath("p[1]/a/b/font")[0].xpath("string(.)")))
+      print("上映时间 "+str(movie.xpath("p[1]/span/font/text()")[0]))
+      print("电影别名 " +str(movie.xpath("p[2]/a/text()")))
+      print("豆瓣评分 "+str(movie.xpath("p[4]/strong/text()")))
 
 if __name__=="__main__":
   bt_spider = BtSpider()
