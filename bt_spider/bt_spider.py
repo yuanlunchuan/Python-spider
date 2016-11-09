af043f77766783d4e0b692b34604091a4dcffe78
Yuan Lunchuan <799652013@qq.com>
2016-11-09 23:51:12 +0800

    1, Add time.

--- bt_spider/bt_spider.py
+++ bt_spider/bt_spider.py
@@ -2,9 +2,11 @@ import requests
 from lxml import etree
 
 class BtSpider:
+  current_page = 1
+
   def get_urls(self):
     urls = []
-    for i in range(1, 2):
+    for i in range(1, 3):
       urls.append("http://www.bttiantang.org/index-"+str(i)+".html")
     return urls
 
@@ -15,7 +17,7 @@ class BtSpider:
     selector = etree.HTML(html_page)
     movies = selector.xpath("//div[@class='title']")
     for movie in movies:
-      print("---------电影名称： "+str(movie.xpath("p[1]/a/b/font")[0].xpath("string(.)")))
+      print(str(movie.xpath("p[1]/a/b/font")[0].xpath("string(.)"))+"  "+str(movie.xpath("p[1]/span/font/text()")[0]))
 
 if __name__=="__main__":
   bt_spider = BtSpider()
