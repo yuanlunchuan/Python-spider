96c74f6cf5ffa014f141b18522257766b53faf39
Yuan Lunchuan <799652013@qq.com>
2016-11-14 19:12:11 +0800

    1, Add .

--- bt_spider/bt_spider.py
+++ bt_spider/bt_spider.py
@@ -7,12 +7,13 @@ class BtSpider:
   client = MongoClient("localhost", 27017)
 
   def __init__(self):
-    db = self.client.bt_sources
-    self.collection = db.bts
+    self.db = self.client.bt_sources
+    self.collection = self.db.bts
 
   def get_urls(self):
     urls = []
-    for i in range(1, 3):
+    for i in range(1, 100):
+      print("----------page: "+str(i))
       urls.append("http://www.bttiantang.org/index-"+str(i)+".html")
     return urls
 
@@ -24,14 +25,11 @@ class BtSpider:
     movies = selector.xpath("//div[@class='title']")
     for movie in movies:
       movie_name = str(movie.xpath("p[1]/a/b/font")[0].xpath("string(.)"))
-      #movie_time = str(movie.xpath("p[1]/span/font/text()")[0])
+      movie_time = str(movie.xpath("p[1]/span/font/text()")[0])
+      alis_name = str(movie.xpath("p[2]/a/text()"))
+      score = str(movie.xpath("p[4]/strong/text()"))
 
-      self.collection.insert({movie_name: movie_name})
-
-      print("电影名称 "+str(movie.xpath("p[1]/a/b/font")[0].xpath("string(.)")))
-      print("上映时间 "+str(movie.xpath("p[1]/span/font/text()")[0]))
-      # print("电影别名 " +str(movie.xpath("p[2]/a/text()")))
-      # print("豆瓣评分 "+str(movie.xpath("p[4]/strong/text()")))
+      self.collection.insert({'movie_name': movie_name, 'movie_time': movie_time, 'alis_name': alis_name, 'score': score})
 
 if __name__=="__main__":
   bt_spider = BtSpider()
