60579aa148d4d4c9bc313e2266a35aa0e87da281
Yuan Lunchuan <799652013@qq.com>
2016-11-14 19:04:03 +0800

    1, Add mongdb config.

--- bt_spider/bt_spider.py
+++ bt_spider/bt_spider.py
@@ -4,6 +4,11 @@ from pymongo import MongoClient
 
 class BtSpider:
   current_page = 1
+  client = MongoClient("localhost", 27017)
+
+  def __init__(self):
+    db = self.client.bt_sources
+    self.collection = db.bts
 
   def get_urls(self):
     urls = []
@@ -18,19 +23,17 @@ class BtSpider:
     selector = etree.HTML(html_page)
     movies = selector.xpath("//div[@class='title']")
     for movie in movies:
-      print("--------------------------")
+      movie_name = str(movie.xpath("p[1]/a/b/font")[0].xpath("string(.)"))
+      #movie_time = str(movie.xpath("p[1]/span/font/text()")[0])
+
+      self.collection.insert({movie_name: movie_name})
+
       print("电影名称 "+str(movie.xpath("p[1]/a/b/font")[0].xpath("string(.)")))
       print("上映时间 "+str(movie.xpath("p[1]/span/font/text()")[0]))
-      print("电影别名 " +str(movie.xpath("p[2]/a/text()")))
-      print("豆瓣评分 "+str(movie.xpath("p[4]/strong/text()")))
+      # print("电影别名 " +str(movie.xpath("p[2]/a/text()")))
+      # print("豆瓣评分 "+str(movie.xpath("p[4]/strong/text()")))
 
-# if __name__=="__main__":
-#   bt_spider = BtSpider()
-#   for url in bt_spider.get_urls():
-#     bt_spider.get_btn_item(bt_spider.html_page_source(url))
-
-client = MongoClient("localhost", 27017)
-db = client.huaxiHospital
-collection = db.Doctor
-for data in collection.find():
-  print("----data: "+str(data))
+if __name__=="__main__":
+  bt_spider = BtSpider()
+  for url in bt_spider.get_urls():
+    bt_spider.get_btn_item(bt_spider.html_page_source(url))
